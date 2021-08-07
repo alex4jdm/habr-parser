4d1310fc21796508379bdfdd5f6547569d6fa357
Alex <alex4jdm@gmail.com>
2021-08-07 18:18:28 +0300

    Added cfg and mongodb init.

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 const Parser = require('./src/parser');
+const mongoClient = require("mongodb").MongoClient;
 
-const miner = new Parser('https://habr.com/');
-miner.start();
+const cfg = require('config.json');
+const scraper = new Parser(cfg.domain);
+
+mongoClient.connect(function(err, client){
+    scraper.init(client, cfg.dbname);
+    const collection = db.collection("users");
+    let user = {name: "Tom", age: 23};
+    collection.insertOne(user, function(err, result){
+          
+        if(err){ 
+            return console.log(err);
+        }
+        console.log(result.ops);
+        client.close();
+    });
+});
--- src/parser.js
+++ src/parser.js
@@ -1,15 +1,22 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const mongodb = require('mongodb');
 
 class Parser {
     constructor(domain) {
         this.url = domain;
     }
 
-    async start() {
+    async init(MongoClient, dbname) {
+        this.client = MongoClient;
+        this.db = this.client.db(dbname);
         this.browser = await puppeteer.launch({headless: false});
         this.page = await this.browser.newPage();
         this.page.goto(this.url, { waitUntil: 'networkidle2' });
+    }
+
+    async startScrapper() {
+
     }
 }
 
